b1ecc942f755ada681ac28ba5dd7789934b2a00e
ZQWhiting <zach.whiting@icloud.com>
2020-11-10 14:54:09 -0700

    created Home page; created Ride page; added background

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import { VideoLink } from '../components/Video';
+import './Home.css';
+
+function Home() {
+	return (
+		<div className='flex container'>
+			<VideoLink />
+			<VideoLink />
+			<VideoLink />
+			<VideoLink />
+			<VideoLink />
+			<VideoLink />
+			<VideoLink />
+		</div>
+	);
+}
+
+export default Home;
