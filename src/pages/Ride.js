0e0fb8710e9fa86267e36eb18d214050234e80d3
ZQWhiting <zach.whiting@icloud.com>
2020-11-12 14:40:06 -0700

    added link to home

--- src/pages/Ride.js
+++ src/pages/Ride.js
@@ -1,5 +1,5 @@
 import { Video } from '../components/Video';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import ridesData from '../assets/rides';
 import './Ride.css';
 
@@ -8,6 +8,9 @@ function Ride() {
 	const { name } = ridesData.find((ride) => ride.id === id);
 	return (
 		<div className='container'>
+			<Link to='/'>
+				<p className='align-right select-hover'>Back to Home</p>
+			</Link>
 			<h1 className='pb-5'>{name}</h1>
 			<div className='flex align-column'>
 				<div className='video'>
